34b4652b72c61e3f1f1da7ebe6d5b818e311338e
Max White <maxjwhite22@gmail.com>
2015-07-28 17:22:11 -0400

    pudge

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -5,13 +5,13 @@ class PokemonController < ApplicationController
   end
 
   def new
-    puts params.to_yaml
-    p = Pokemon.new
-
-    p.name = params["name"]
-    p.type = params["type"]
-    p.image_uri = params["image_uri"]
-    p.resource_uri = params["resource_uri"]
+    pk = params["pokemon"]
+    Pokemon.create(
+      name: pk["name"],
+      types: pk["types"],
+      image_uri: pk["image_uri"],
+      resource_uri: pk["resource_uri"]
+    )
     head :no_content
   end
 end
