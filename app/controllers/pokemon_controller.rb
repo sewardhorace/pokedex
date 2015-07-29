60fc032be49c5885c92a2e90476d4c2c2fbb8fb2
mikekavouras <kavourasm@gmail.com>
2015-07-29 10:45:47 -0400

    key is pokemon

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,7 +2,7 @@ class PokemonController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    render json: Pokemon.select("name, types, image_uri, resource_uri, id").all
+    render json: {pokedex: Pokemon.select("name, types, image_uri, resource_uri, id").all}
   end
 
   def new
