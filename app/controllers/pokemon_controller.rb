bb71c8787ea4144fcc1b9a6c3be6b8e1ca462487
Max White <maxjwhite22@gmail.com>
2015-07-28 20:46:40 -0400

    explicitly set pokemon id

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,12 +2,13 @@ class PokemonController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    render json: Pokemon.select("name, types, image_uri, resource_uri").all
+    render json: Pokemon.select("name, types, image_uri, resource_uri, id").all
   end
 
   def new
     pk = params["pokemon"]
     Pokemon.create(
+      id: pk["id"],
       name: pk["name"],
       types: pk["types"],
       image_uri: pk["image_uri"],
